01668de0cfe3cda762d233403296f05348470f13
Matt Miller <matt@m11r.dev>
2025-10-19 23:31:31 +0100

    ci: attempting github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://matt123miller.github.io",
+  base: "vafo-test",
+});
